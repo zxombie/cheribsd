d8b72b63c68a89c9cc10c7a295d9ed7da1357cde
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2017-09-13 10:27:32 +0100

    Fix build of cross tools

--- tools/build/mk/bsd.lib.mk
+++ tools/build/mk/bsd.lib.mk
@@ -1,4 +1,8 @@
 # $FreeBSD$
 
-.include "../../../share/mk/bsd.lib.mk"
 .include "Makefile.boot"
+# .if ${HOST_OS} == "FreeBSD"
+.include "../../../share/mk/bsd.lib.mk"
+# .else
+# .include <bsd.lib.mk>
+# .endif
--- tools/build/mk/bsd.prog.mk
+++ tools/build/mk/bsd.prog.mk
@@ -1,4 +1,8 @@
 # $FreeBSD$
 
-.include "../../../share/mk/bsd.prog.mk"
 .include "Makefile.boot"
+# .if ${HOST_OS} == "FreeBSD"
+.include "../../../share/mk/bsd.prog.mk"
+# .else
+# .include <bsd.prog.mk>
+# .endif
