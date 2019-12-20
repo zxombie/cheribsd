b2e249d18430c1c2b1641b51c9ccc32c2facaec7
Nathaniel Filardo <nwf20@cl.cam.ac.uk>
2019-12-20 14:08:29 +0000

    fix vm_page_replace post-merge

--- sys/vm/vm_page.c
+++ sys/vm/vm_page.c
@@ -1741,7 +1741,7 @@ vm_page_replace(vm_page_t mnew, vm_object_t object, vm_pindex_t pindex)
 	/*
 	 * Copy across capability tracking flags
 	 */
-	if (mold->aflags & PGA_CAPSTORED)
+	if (mold->a.flags & PGA_CAPSTORED)
 		vm_page_capdirty(mnew);
 	if (mold->oflags & VPO_PASTCAPSTORE)
 		mnew->oflags |= VPO_PASTCAPSTORE;
