c29475a580f861007747136eaa180beaa2fcc3f2
Nathaniel Filardo <nwf20@cl.cam.ac.uk>
2019-12-23 11:06:48 +0000

    [TEMPORARY] Cease using sentries in CHERI rtld.
    There appears to be some wacky relocations in C++, causing seal
    violations during program load.  Temporarily take the easy way out.

--- libexec/rtld-cheri-elf/mips/rtld_machdep.h
+++ libexec/rtld-cheri-elf/mips/rtld_machdep.h
@@ -174,10 +174,12 @@ make_code_pointer(const Elf_Sym *def, const struct Struct_Obj_Entry *defobj,
 		dbg_assert(defobj->cheri_captable_abi == DF_MIPS_CHERI_ABI_PCREL);
 	}
 	/* All code pointers should be sentries: */
+#ifdef notyet
 #if __has_builtin(__builtin_cheri_seal_entry)
 	ret = __builtin_cheri_seal_entry(ret);
 #else
 #warning "__builtin_cheri_seal_entry not supported, please update LLVM"
+#endif
 #endif
 	return __DECONST(dlfunc_t, ret);
 }
